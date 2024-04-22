4eff0c480e173dadc90af14da36fbcc1036b53fd
SachaPapiernik <sacha.papiernik@gmail.com>
2024-04-22 12:20:45 +0200

    added move model

--- pokermon_api_app/models.py
+++ pokermon_api_app/models.py
@@ -11,3 +11,23 @@ class Item(models.Model):
 
     class Meta:
         db_table = 'items'
+
+class Move(models.Model):
+    id = models.AutoField(primary_key=True)
+    identifier = models.CharField(max_length=79)
+    generation_id = models.IntegerField()
+    type_id = models.IntegerField()
+    power = models.SmallIntegerField(null=True, blank=True)
+    pp = models.SmallIntegerField(null=True, blank=True)
+    accuracy = models.SmallIntegerField(null=True, blank=True)
+    priority = models.SmallIntegerField()
+    target_id = models.IntegerField()
+    damage_class_id = models.IntegerField()
+    effect_id = models.IntegerField()
+    effect_chance = models.IntegerField(null=True, blank=True)
+    contest_type_id = models.IntegerField(null=True, blank=True)
+    contest_effect_id = models.IntegerField(null=True, blank=True)
+    super_contest_effect_id = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'moves'
