4709d1851e1f6f0ce1d34fe875c978914e0f489a
SachaPapiernik <sacha.papiernik@gmail.com>
2024-04-22 12:26:34 +0200

    added move view

--- pokermon_api_app/views.py
+++ pokermon_api_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_http_methods, require_GET
-from .models import Item
+from .models import Item, Move
 from django.forms.models import model_to_dict
 from django.shortcuts import get_object_or_404
 import json
+
+# Item
 
 @require_GET
 def item_list(request):
@@ -31,4 +33,30 @@ def item_detail(request, id):
         item.save()
         return JsonResponse(model_to_dict(item))
 
-# Create your views here.
+# Move
+
+@require_GET
+def item_list(request):
+    # Fetch all items
+    items = Move.objects.all()
+    items_list = [model_to_dict(item) for item in items]
+    return JsonResponse(items_list, safe=False)
+
+@require_http_methods(["GET", "PUT", "DELETE"])
+def move_detail(request, id):
+    move = get_object_or_404(Move, pk=id)
+
+    if request.method == 'GET':
+        return JsonResponse(model_to_dict(move))
+
+    elif request.method == 'DELETE':
+        move.delete()
+        return HttpResponse(status=204)
+    
+    elif request.method == 'PUT':
+        data = json.loads(request.body)
+        for field, value in data.items():
+            setattr(move, field, value)
+        move.save()
+        return JsonResponse(model_to_dict(move))
+
