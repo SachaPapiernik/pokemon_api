808bda310a8971cdddf6a393d7b341d786b53710
SachaPapiernik <sacha.papiernik@gmail.com>
2024-04-22 09:50:15 +0200

    added Item model

--- pokermon_api_app/models.py
+++ pokermon_api_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    identifier = models.CharField(max_length=79)
+    category_id = models.IntegerField()
+    cost = models.IntegerField()
+    fling_power = models.IntegerField(null=True)
+    fling_effect_id = models.IntegerField(null=True)
+
+    class Meta:
+        db_table = 'items'
